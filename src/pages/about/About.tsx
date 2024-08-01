300c788f761cbb7d19d7a24ff53b205fb81b3aff
Ethan Logue <ethanlogue03@gmail.com>
2024-07-31 23:41:29 -0400

    added line art, has responsive bugs

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './About.css';
 import Page from '../../components/Page';
 import SkillStack from './SkillStack';
+import LineArt from './LineArt';
 
 const About: React.FC = () => {
     return (
@@ -10,10 +11,14 @@ const About: React.FC = () => {
                 <div className='about-info'>
                     <h2 className='page-title about-title'>About Me</h2>
                     <p className='about-bio'>Hello there! I'm a fourth-year web and mobile undergraduate at RIT.</p>
-                    <p className='about-bio'>I'm currently working at <a className='about-link' href='https://www.synapseresults.com/'>Synapse Marketing Solutions</a> as a Full-Stack Web Developer, where I am following my passion for front-end web development. While I initially pursued a degree in IT, my journey led me to realize my true passion lies in creating beautiful user experiences through web development. Blending my technical experience from my IT jobs with my passion for coding and creativity, transitioning to a Web & Mobile Computing degree was a natural fit. I'm committed to creating stylistic, consistent, and intuitive user interfaces for clients and improving myself as I do so.</p>
+                    <p className='about-bio'>I'm currently working at <a className='about-link' target='_blank' href='https://www.synapseresults.com/'>Synapse Marketing Solutions</a> as a Full-Stack Web Developer, where I am following my passion for front-end web development. While I initially pursued a degree in IT, my journey led me to realize my true passion lies in creating beautiful user experiences through web development. Blending my technical experience from my IT jobs with my passion for coding and creativity, transitioning to a Web & Mobile Computing degree was a natural fit. I'm committed to creating stylistic, consistent, and intuitive user interfaces for clients and improving myself as I do so.</p>
                 </div>
                 <div className='about-skills'>
                     <SkillStack />
+                </div>
+
+                <div className='about-line-art'>
+                    <LineArt />
                 </div>
     
             </div>

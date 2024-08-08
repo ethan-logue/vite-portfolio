f1dfcd2099d72db373754884255f9e6e836aadd8
Ethan Logue <ethanlogue03@gmail.com>
2024-08-07 22:46:42 -0400

    great progress with background accent

--- src/pages/home/BackgAccent.tsx
+++ src/pages/home/BackgAccent.tsx
@@ -7,8 +7,7 @@ interface BackgAccentProps {
 const BackgAccent: React.FC<BackgAccentProps> = ({ parent }) => {
 
     const keywords = [
-        'Ethan',
-        'Logue',
+        'Ethan Logue',
         'Web',
         'Developer',
         'CSS',
@@ -18,58 +17,69 @@ const BackgAccent: React.FC<BackgAccentProps> = ({ parent }) => {
         'RIT',
         'Outdoors',
         'Frontend',
-        'Music',
+        'Music'
     ];
 
+    console.log(keywords);
+    
+
     const containerRef = useRef<HTMLDivElement>(null);
+    const pContainer = document.getElementById(parent);
+    const cWidth = pContainer?.clientWidth ?? 0;
+    const cHeight = pContainer?.clientHeight ?? 0;
 
-    // useEffect(() => {
-    //     const pContainer = document.getElementById(parent);
-    //     const cWidth = pContainer?.clientWidth ?? 0;
-    //     const cHeight = pContainer?.clientHeight ?? 0;
+    useEffect(() => {
 
-    //     if (!containerRef.current) return;
+        // mobile check
+        if (cWidth <= 768) return;
+        
+        if (!containerRef.current) return;
+        const container = containerRef.current;
 
-    //     const container = containerRef.current;
+        const createKeywordElement = (keyword: string) => {
+            const span = document.createElement('span');
+            span.textContent = keyword;
+            return span;
+        };
 
-    //     let totalHeight = 0;
-    //     let rowDirection = 'right';
+        const createRow = () => {
+            const row = document.createElement('div');
+            row.classList.add('scroll-row');
+            row.style.animationDuration = `${Math.random() * (22 - 18) + 18}s`;
+            row.style.animationDelay = `${Math.random() * (0 - 3) + 3}s`;
+            row.style.opacity = `${Math.random() * (0.4 - 0.1) + 0.1}`;
+            return row;
+        }
 
-    //     const createKeywordElement = (keyword: string) => {
-    //         const span = document.createElement('span');
-    //         span.textContent = keyword;
-    //         span.style.animationDuration = `${10 + Math.random() * 5}s`; // Random duration between 10 and 15 seconds
-    //         return span;
-    //     };
+        const randomizeKeywords = () => {
+            for (let i = keywords.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [keywords[i], keywords[j]] = [keywords[j], keywords[i]];
+            }
+        }
+        
+        const rowHeight = 38;        
+        let keywordIndex = 0;
+        let totalHeight = 0;
+        
+        while (totalHeight < cHeight - rowHeight) {
+            randomizeKeywords();
+            const currentRow = createRow();
+            
+            for (let j = 0; j < 10; j++) {
+                if (keywordIndex >= keywords.length) {
+                    keywordIndex = 0;
+                }
+                
+                const keywordElement = createKeywordElement(keywords[keywordIndex]);
+                currentRow.appendChild(keywordElement);
+                keywordIndex++;
+            }
 
-    //     let currentRow = document.createElement('div');
-    //     currentRow.classList.add('scroll-row');
-    //     container.appendChild(currentRow);
-
-    //     let keywordIndex = 0;
-
-    //     const rowHeight = cHeight * 0.1;
-
-    //     while (totalHeight < cHeight) {
-    //         const keywordElement = createKeywordElement(keywords[keywordIndex]);
-    //         currentRow.appendChild(keywordElement);
-
-    //         if (currentRow.scrollWidth >= cWidth) {
-    //             totalHeight += rowHeight;
-    //             currentRow.style.animationDirection = rowDirection;
-
-    //             rowDirection = rowDirection === 'right' ? 'left' : 'right';
-
-    //             if (totalHeight < cHeight) {
-    //                 currentRow = document.createElement('div');
-    //                 currentRow.classList.add('scroll-row');
-    //                 container.appendChild(currentRow);
-    //             }
-    //         }
-
-    //         keywordIndex = (keywordIndex + 1) % keywords.length;
-    //     }
-    // }, [parent, keywords]);
+            container.appendChild(currentRow);            
+            totalHeight += rowHeight;
+        }
+    }, [cWidth, cHeight]);
 
     return <div ref={containerRef} className="scroll-container"></div>;
 };
